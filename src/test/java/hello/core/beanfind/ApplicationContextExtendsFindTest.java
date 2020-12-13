3ce63982ebc980305156c2182fdc8fee681686c0
JayHan88 <han5517@kaist.ac.kr>
2020-12-13 22:56:27 +0900

    automatic DI

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -27,7 +27,7 @@ public class ApplicationContextExtendsFindTest {
 	@Test
 	@DisplayName("부모 타입으로 조회 시 자식이 둘 이상 있으면 빈 이름을 지정하면 된다.")
 	void findBeanByParentTypeBeanName() {
-		DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicfy", DiscountPolicy.class);
+		DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
 		assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
 	}
 
